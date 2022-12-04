2d8dd15069b3b15ba2d3591762193ef03282c80d
j3s30p <jeseoppark@j3s30p.local>
2022-12-05 05:20:37 +0900

    Coin Tracker

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import { useEffect, useState } from "react";
 
 function App() {
     const [loading, setLoading] = useState(true);
-    const [selected, setSelected] = useState(1);
+
     const [coins, setCoins] = useState([]);
-    const [money, setMoney] = useState(0);
-    const onChangeInput = (event) => setMoney(event.target.value);
+
     const onSubmit = (event) => {
         event.preventDefault();
     };
-    const onChangeSelect = (event) => {
-        setSelected(parseInt(event.target.value));
-    };
+
     useEffect(() => {
         fetch("https://api.coinpaprika.com/v1/tickers")
             .then((response) => response.json())
@@ -27,38 +24,14 @@ function App() {
                 <strong>loading...</strong>
             ) : (
                 <div>
-                    <select onChange={onChangeSelect}>
+                    <select>
                         {coins.map((coin) => (
-                            <option key={coin.id} value={coin.rank}>
+                            <option key={coin.id}>
                                 {coin.name} ({coin.symbol}):{" "}
                                 {coin.quotes.USD.price} USD
                             </option>
                         ))}
                     </select>
-                    <hr />
-                    <form onSubmit={onSubmit}>
-                        <input
-                            onChange={onChangeInput}
-                            value={money === 0 ? "" : money}
-                            type="number"
-                            placeholder="Write your USD"
-                        ></input>
-                        <button>EXCHANGE</button>
-                    </form>
-                    <hr />
-                    <h3>{money} USD</h3>
-                    <h3>EQUALS</h3>
-                    {coins.map((coin) => {
-                        if (coin.rank === selected) {
-                            return (
-                                <h3 key={coin.id}>
-                                    {money / coin.quotes.USD.price}{" "}
-                                    {coin.symbol}
-                                </h3>
-                            );
-                        }
-                        return;
-                    })}
                 </div>
             )}
         </div>
